a0a2288a8be6e6d1ce9bb8345b7d46a195d97efe
Andrew Y <andrew@compandsave.com>
2020-07-29 11:49:27 -0700

    added algo and cas-axios

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
+const algo = require('./lib/algo.js');
+const caxios = require('./lib/cas-axios.js');
 const jwt = require('./lib/jwt.js');
 const Log = require('./lib/log.js');
 
-module.exports = { jwt, Log };
+module.exports = { algo, caxios, jwt, Log };
