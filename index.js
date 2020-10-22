d309a57457f3ea627bd7789383ad3bf10b17b9d4
Andrew Y <andrew@compandsave.com>
2020-10-22 14:37:39 -0700

    add timer

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const algo = require('./lib/algo.js');
 const caxios = require('./lib/cas-axios.js');
 const jwt = require('./lib/jwt.js');
+const timer = require('./lib/timer.js');
 const Log = require('./lib/log.js');
 
-module.exports = { algo, caxios, jwt, Log };
+module.exports = { algo, caxios, jwt, timer, Log };
